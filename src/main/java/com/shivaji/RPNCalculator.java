ad7bc3cd21049fb0f2ca8833e92bda064ca8786d
Shivaji Byrapaneni <shivaji.byrapaneni@aia.com>
2018-05-04 15:13:36 +1000

    1) Minified a logic 2) Updated Test message

--- src/main/java/com/shivaji/RPNCalculator.java
+++ src/main/java/com/shivaji/RPNCalculator.java
@@ -26,141 +26,131 @@ import org.slf4j.LoggerFactory;
  */
 public class RPNCalculator {
 
-  private static final Logger LOG = LoggerFactory.getLogger(RPNCalculator.class);
+    private static final Logger LOG = LoggerFactory.getLogger(RPNCalculator.class);
 
-  private final RPNStack<BigDecimal> stack;
+    private final RPNStack<BigDecimal> stack;
 
-  public static void main(String[] args) {
-    LOG.info(SPACE);
-    LOG.info("<!---------------------------------------------!>");
-    LOG.info("Use exit and hit enter to exit the program");
-    LOG.info("Enter any real number with valid operation +, -, *, /, sqrt, undo, clear");
-    LOG.info("No Real and not acceptable operations are ignored.");
-    LOG.info("<!---------------------------------------------!>");
-    LOG.info(SPACE);
-    Scanner scan = new Scanner(System.in);
-    String input = null;
-    RPNCalculator rpnCalculator = new RPNCalculator(new RPNStack<>());
-    while (!isExit(input)) {
-      input = scan.nextLine();
-      String resultStack = rpnCalculator.calculate(input);
-      LOG.info("Stack: {}", resultStack);
-    }
-  }
-
-  private static boolean isExit(String input) {
-    return StringUtils.equalsIgnoreCase(input, "exit");
-  }
-
-  public RPNCalculator(RPNStack<BigDecimal> stack) {
-    this.stack = stack;
-  }
-
-  public String calculate(String inputStr) {
-    if (isExit(inputStr)) {
-      return getStackForDisplay();
-    }
-    List<String> tokens = parseAndCleanseInputStr(inputStr);
-    LOG.info("Input considered after cleansing --> {}", StringUtils.join(tokens, SPACE));
-    boolean continueWithNextItem = Boolean.TRUE;
-
-    // Using legacy loop to mitigate with breaking the flow
-    // if any operation is not possible
-    // Throwing exception is expensive within forEach
-    for (int index = 0; index < tokens.size(); index++) {
-
-      if (!continueWithNextItem) {
-        break;
-      }
-
-      String token = tokens.get(index);
-
-      if (NumberUtils.isCreatable(token)) { // Number
-        stack.pushWithStashing(Optional.of(new BigDecimal(token)));
-      } else { // Operation
-        Optional<Operation> parse = Operation.parse(token);
-        // No Point is checking is present, as cleansing has confirmed this
-        switch (parse.get()) {
-          case UNDO:
-            continueWithNextItem = undo(continueWithNextItem, index);
-            break;
-          case CLEAR:
-            stack.stashAndclear(); // Clear is always possible
-            break;
-          case ADD:
-            continueWithNextItem = mathOperation(index, Operation.ADD);
-            break;
-          case SUB:
-            continueWithNextItem = mathOperation(index, Operation.SUB);
-            break;
-          case MUL:
-            continueWithNextItem = mathOperation(index, Operation.MUL);
-            break;
-          case DIV:
-            continueWithNextItem = mathOperation(index, Operation.DIV);
-            break;
-          case SQRT:
-            continueWithNextItem = mathOperation(index, Operation.SQRT);
-            break;
+    public static void main(String[] args) {
+        LOG.info(SPACE);
+        LOG.info("<!---------------------------------------------!>");
+        LOG.info("Use exit and hit enter to exit the program");
+        LOG.info("Enter any real number with valid operation +, -, *, /, sqrt, undo, clear");
+        LOG.info("No Real and not acceptable operations are ignored.");
+        LOG.info("<!---------------------------------------------!>");
+        LOG.info(SPACE);
+        Scanner scan = new Scanner(System.in);
+        String input = null;
+        RPNCalculator rpnCalculator = new RPNCalculator(new RPNStack<>());
+        while (!isExit(input)) {
+            input = scan.nextLine();
+            String resultStack = rpnCalculator.calculate(input);
+            LOG.info("Stack: {}", resultStack);
         }
-      }
     }
 
-    return getStackForDisplay();
-  }
+    private static boolean isExit(String input) {
+        return StringUtils.equalsIgnoreCase(input, "exit");
+    }
 
-  private boolean mathOperation(int index, Operation operation) {
-    if (operation.equals(Operation.SQRT)) {
-      if (stack.hasAtleastOneItem()) {
-        stack.stash();
-        stack.pushWithoutStashing(
-            Optional.of(OperationCalc.doCalc(operation, stack.popWithOutStashing().get())));
-      } else {
+    public RPNCalculator(RPNStack<BigDecimal> stack) {
+        this.stack = stack;
+    }
+
+    public String calculate(String inputStr) {
+        if (isExit(inputStr)) {
+            return getStackForDisplay();
+        }
+        List<String> tokens = parseAndCleanseInputStr(inputStr);
+        LOG.info("Input considered after cleansing --> {}", StringUtils.join(tokens, SPACE));
+        boolean continueWithNextItem = Boolean.TRUE;
+
+        // Using legacy loop to mitigate with breaking the flow
+        // if any operation is not possible
+        // Throwing exception is expensive within forEach
+        for (int index = 0; index < tokens.size(); index++) {
+
+            if (!continueWithNextItem) {
+                break;
+            }
+
+            String token = tokens.get(index);
+
+            if (NumberUtils.isCreatable(token)) { // Number
+                stack.pushWithStashing(Optional.of(new BigDecimal(token)));
+            }
+            else { // Operation
+                Optional<Operation> parse = Operation.parse(token);
+                // No Point is checking is present, as cleansing has confirmed this
+                switch (parse.get()) {
+                    case UNDO:
+                        continueWithNextItem = undo(continueWithNextItem, index);
+                        break;
+                    case CLEAR:
+                        stack.stashAndclear(); // Clear is always possible
+                        break;
+                    case ADD:
+                        continueWithNextItem = mathOperation(index, Operation.ADD);
+                        break;
+                    case SUB:
+                        continueWithNextItem = mathOperation(index, Operation.SUB);
+                        break;
+                    case MUL:
+                        continueWithNextItem = mathOperation(index, Operation.MUL);
+                        break;
+                    case DIV:
+                        continueWithNextItem = mathOperation(index, Operation.DIV);
+                        break;
+                    case SQRT:
+                        continueWithNextItem = mathOperation(index, Operation.SQRT);
+                        break;
+                }
+            }
+        }
+
+        return getStackForDisplay();
+    }
+
+    private boolean mathOperation(int index, Operation operation) {
+        boolean isSqrtOperation = operation.equals(Operation.SQRT);
+        if (stack.hasAtleastTwoItems() || (isSqrtOperation && stack.hasAtleastOneItem())) {
+            stack.stash();
+            stack.pushWithoutStashing(
+                Optional.of(
+                    OperationCalc.doCalc(
+                        operation,
+                        stack.popWithOutStashing().get(),
+                        isSqrtOperation ? null : stack.popWithOutStashing().get())));
+            return Boolean.TRUE;
+        }
         inSufficientParametersMsg(operation.getVal(), index);
         return Boolean.FALSE;
-      }
-    } else {
-      if (stack.hasAtleastTwoItems()) {
-        stack.stash();
-        stack.pushWithoutStashing(
-            Optional.of(
-                OperationCalc.doCalc(
-                    operation,
-                    stack.popWithOutStashing().get(),
-                    stack.popWithOutStashing().get())));
-      } else {
-        inSufficientParametersMsg(operation.getVal(), index);
-        return Boolean.FALSE;
-      }
     }
-    return Boolean.TRUE;
-  }
 
-  private boolean undo(boolean continueWithNextItem, int index) {
-    boolean isUndoSuccess = stack.undo();
-    if (!isUndoSuccess) {
-      inSufficientParametersMsg(Operation.UNDO.getVal(), index);
-      continueWithNextItem = false;
+    private boolean undo(boolean continueWithNextItem, int index) {
+        boolean isUndoSuccess = stack.undo();
+        if (!isUndoSuccess) {
+            inSufficientParametersMsg(Operation.UNDO.getVal(), index);
+            continueWithNextItem = false;
+        }
+        return continueWithNextItem;
     }
-    return continueWithNextItem;
-  }
 
-  private String getStackForDisplay() {
-    List<String> stackList =
-        stack
-            .list()
-            .stream()
-            .map(RPNUtil.bigDecimalWith10DecimalPlacesStr)
-            .collect(Collectors.toList());
-    return StringUtils.join(stackList, SPACE);
-  }
+    private String getStackForDisplay() {
+        List<String> stackList =
+            stack
+                .list()
+                .stream()
+                .map(RPNUtil.bigDecimalWith10DecimalPlacesStr)
+                .collect(Collectors.toList());
+        return StringUtils.join(stackList, SPACE);
+    }
 
-  private void inSufficientParametersMsg(String operation, int index) {
-    int position = (index + 1) * 2 - 1; // considering spaces between operations as well
-    //    int position = ( index + 1); // This is ideal position of opeartion however in problem
-    // statement example it sounded like they wanted above as there in a ambigeous requirement
-    // statement about  position
+    private void inSufficientParametersMsg(String operation, int index) {
+        int position = (index + 1) * 2 - 1; // considering spaces between operations as well
+        //    int position = ( index + 1); // This is ideal position of opeartion however in problem
+        // statement example it sounded like they wanted above as there in a ambigeous requirement
+        // statement about  position
 
-    LOG.warn("operator {} (position: {}): insufficient parameters", operation, position);
-  }
+        LOG.warn("operator {} (position: {}): insufficient parameters", operation, position);
+    }
 }
